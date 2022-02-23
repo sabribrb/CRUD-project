d7e62834ba66b9264fd96e46a2b03b6001b1f5de
sabrina <sabribarbiero@hotmail.com>
2022-02-22 22:57:42 -0300

    destroy and edit photo, header and footer created

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os #modulo de sistema op. para eliminar la foto y updatearla
 from flask import Flask
 from flask import render_template, request,redirect #los request son solicitud de info, redirect permite redireccionar
 from flaskext.mysql import MySQL
@@ -12,6 +13,9 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sysempleados'
 mysql.init_app(app)
 
+#referenciar a la carpeta donde se subiran las fotos, con la libreria os
+CARPETA=os.path.join('uploads')
+app.config['CARPETA']=CARPETA
 
 #para abrir el index vamos a la terminal : "python app.py"
 @app.route("/") #indicamos la redireccion del url
@@ -28,19 +32,67 @@ def index():
     return render_template('empleados/index.html', empleados=empleados)
 
 
-@app.route('/create') #cuando llamemos a create nos redigira a ese html
-def create():
-    return render_template('empleados/create.html')
+
 @app.route('/destroy/<int:id>')
 def destroy(id):
     conn= mysql.connect()
     cursor= conn.cursor()
+
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+    ubicacion= cursor.fetchall() #lo convierte en tupla pero necesitamos solo la [0][0]
+    os.remove(os.path.join(app.config['CARPETA'], ubicacion[0][0]))  
+        
     cursor.execute("DELETE FROM `sysempleados`.`empleados` WHERE id=%s ", (id))
     conn.commit()
     return redirect('/')
 
+@app.route('/create') #cuando llamemos a create nos redigira a ese html
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre= request.form['txtNombre']
+    _correo= request.form['txtCorreo']
+    _foto= request.files['txtFoto']
+    id= request.form['txtID']
+
+    sql= "UPDATE `sysempleados`.`empleados` SET `nombre`=%s, `correo`=%s WHERE id=%s ;"
+    datos= (_nombre, _correo, id)
+
+    conn= mysql.connect()
+    cursor= conn.cursor()
+    now= datetime.now()
+    tiempo= now.strftime("%Y%H%M%S") #año-hora-min-seg
+
+  
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename 
+        _foto.save("uploads/"+nuevoNombreFoto) 
+        cursor.execute("SELECT foto FROM `sysempleados`.`empleados` WHERE id=%s", id) 
+        fila= cursor.fetchall() 
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0])) 
+        cursor.execute("UPDATE `sysempleados`.`empleados` SET foto=%s WHERE id=%s;", (nuevoNombreFoto, id)) 
+        conn.commit() 
+
+    cursor.execute(sql, datos) 
+    conn.commit() 
+    return redirect('/')
+
+
+@app.route('/edit/<int:id>')
+def edit(id):  
+    conn= mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute("SELECT * FROM `sysempleados`.`empleados` WHERE id=%s ", (id))
+    empleados= cursor.fetchall() #convierte a tupla los datos
+    conn.commit()
+    return render_template('empleados/edit.html', empleados=empleados)
+
+
+
 @app.route('/store', methods=["POST"]) #redireccionamiento del form create
-#aclarando queserá de un metodo post
+#aclarando que será de un metodo post
 def storage():
     _nombre= request.form['txtNombre']
     _correo= request.form['txtCorreo']
@@ -48,6 +100,7 @@ def storage():
     #guardar el nombre de la foto con datetime
     now= datetime.now()
     tiempo= now.strftime("%Y%H%M%S") #año-hora-min-seg
+    nuevoNombreFoto="NO PHOTO"
     if _foto.filename!='':
         nuevoNombreFoto= tiempo+_foto.filename
         _foto.save("uploads/"+nuevoNombreFoto)
@@ -59,7 +112,7 @@ def storage():
     cursor.execute(sql, datos) #ejecutamos la sentencia sql 
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return redirect('/')
 
 #esto va al final porque es la ejecucion del programa
 if __name__=='__main__':
